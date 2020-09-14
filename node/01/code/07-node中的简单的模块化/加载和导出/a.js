27af59e64d909f3004defbb1a45f705cb204b4bb
libin <bin27.li@tcl.com>
2020-09-14 21:54:54 +0800

    node导入加载

--- node/01/code/07-node中的简单的模块化/加载和导出/a.js
+++ node/01/code/07-node中的简单的模块化/加载和导出/a.js
@@ -1,3 +1,11 @@
 // 在 Node 中，没有全局作用域，只有模块作用域(外部访问不到内部，内部也访问不到外部)
 // 在每个文件模块中都提供一个对象：exports，exports 默认是个空对象
 // 如果想要访问其它的文件模块中的变量，可以通过 exports 导出，在需要访问的页面通过 require 导入
+
+var foo = "aaaa"
+
+var b = require("./b.js")
+
+console.log(foo)
+
+console.log(b.foo)
